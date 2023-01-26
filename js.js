f19e7d19a8be91636a2b4ef7547b4e1982050288
George-Kuzmin <122080727+George-Kuzmin@users.noreply.github.com>
2023-01-26 19:03:02 +0400

    Working-buttons

--- js.js
+++ js.js
@@ -1,48 +1,69 @@
 
-// находим элементы на странице
 const button = document.getElementById("add_button")
-
 const taskInput = document.getElementById("input")
-
 const taskList = document.getElementById("task_list")
+const clearAllButton = document.getElementById("clear_all")
+const taskCountText = document.getElementsByClassName("task-count-text")[0]
+
+button.addEventListener("click", addTask);
+taskList.addEventListener("click", deleteTask)
+clearAllButton.addEventListener("click", clearAll)
 
 let tasks = [];
 
-// добавляем задачу
-button.addEventListener("click", addTask);
+let storageItems = localStorage.getItem("tasks")
+if (storageItems) {
+    tasks = JSON.parse(storageItems)
+}
 
-// удаляем задачу
-taskList.addEventListener("click", deleteTask)
+tasks.forEach(function (task) {
+    // создаём новый элемент и в него засовываем текст из input
+    const newTaskHTML = document.createElement("div")
+    newTaskHTML.innerHTML = `
+        <div id="${task.id}" class="task-list-item">
+            <p class="task-list-item-text">${task.text}</p>
+            <div class="div-for-button">
+              <button class="edit-task-button btn-action" data-action="edit">
+                <img src="edit-pencil.png" alt="edit" width="22" height="22">
+              </button>
+              <button class="clear-task-button btn-action" data-action="delete">
+                <img src="trash-2-48.png" alt="delete" width="17" height="17"/>
+              </button>
+            </div>
+        </div>
+  `
+    taskList.appendChild(newTaskHTML)
+})
 
 // функции
 function addTask () {
-    // достаём значение input
     const taskText = taskInput.value
 
-    // описываем объект в виде задачи
-    const taskObject = {
+    const newTask = {
         id: Date.now(),
         text: taskText,
-        done: false,
+        done: false
     };
 
-    // добавим задачу в массив с задачами
-    tasks.push(taskObject)
-    console.log(tasks);
+    tasks.push(newTask)
 
+    saveToLocalStorage()
 
-    // создаём новый элемент и в него засовываем текст из input
-    const newTask = document.createElement("div")
-    newTask.innerHTML = `
-        <div class="task-list-item">
-            <p class="task-list-item-text">${taskText.text}</p>
-            <button class="clear-task-button btn-action">
-              <img src="trash-2-48.png" alt="delete" width="17" height="17"/>
-            </button>
+    const newTaskHTML = document.createElement("div")
+    newTaskHTML.innerHTML = `
+        <div id="${newTask.id}" class="task-list-item">
+            <p class="task-list-item-text">${newTask.text}</p>
+            <div class="div-for-button">
+              <button class="edit-task-button btn-action" data-action="edit">
+                <img src="edit-pencil.png" alt="edit" width="22" height="22"> 
+              </button>
+              <button class="clear-task-button btn-action" data-action="delete">
+                <img src="trash-2-48.png" alt="delete" width="17" height="17"/>
+              </button>
+            </div>
         </div>
   `
-
-    taskList.appendChild(newTask)
+    taskList.appendChild(newTaskHTML)
 
     // очищаем поле ввода и возвращаем на него фокус
     taskInput.value = ""
@@ -53,7 +74,48 @@ function deleteTask (event) {
     // проверяем, что клик будет по кнопке удалить
     if (event.target.dataset.action === "delete") {
         const parentNode = event.target.closest(".task-list-item")
+        const id = Number(parentNode.id)
+
+        const index = tasks.findIndex(function (task) {
+            if (task.id === id) {
+                return true
+            }
+        })
+
+        // удаляем задачу из массива
+        tasks.splice(index, 1)
+        saveToLocalStorage()
         parentNode.remove()
     }
 }
+function clearAll (event) {
+    tasks = []
+    saveToLocalStorage()
+    taskList.innerHTML = ""
+}
+function saveToLocalStorage () {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    updateTaskCount()
+}
+function updateTaskCount () {
+    taskCountText.innerText = `You have ${tasks.length} pending tasks`
+}
+updateTaskCount()
 
+function createHtmlElement (task) {
+    const newTaskHTML = document.createElement("div")
+    newTaskHTML.innerHTML = `
+        <div id="${task.id}" class="task-list-item">
+            <p class="task-list-item-text">${task.text}</p>
+            <div class="div-for-button">
+              <button class="edit-task-button btn-action" data-action="edit">
+                <img src="edit-pencil.png" alt="edit" width="22" height="22">
+              </button>
+              <button class="clear-task-button btn-action" data-action="delete">
+                <img src="trash-2-48.png" alt="delete" width="17" height="17"/>
+              </button>
+            </div>
+        </div>
+  `
+    taskList.appendChild(newTaskHTML)
+}
